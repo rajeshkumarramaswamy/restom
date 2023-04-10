bdc06c76fc29fb1745e0a16bd9b76ae951097c6d
rajesh kumar <rajeshkramaswamy@gmail.com>
2023-04-10 10:06:47 +0100

    order by date fix

--- src/utils/services/ReactQueryServices.js
+++ src/utils/services/ReactQueryServices.js
@@ -1,8 +1,11 @@
-import { query, collection, orderBy, doc } from "firebase/firestore";
+import { query, collection, orderBy } from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
 
 export const restaurantsRef = query(collection(db, "restaurants"));
-export const ordersRef = query(collection(db, "orders"));
+export const ordersRef = query(
+  collection(db, "orders"),
+  orderBy("date", "desc")
+);
 
 export const mutationOrdersRef = query(collection(db, "orders"));
 export const driversRef = query(collection(db, "drivers"));
